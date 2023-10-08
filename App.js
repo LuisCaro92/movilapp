39786ae086d6426d19024b1876ca19be767c0c2d
luis caro <luis.carosalgado1@gmail.com>
2023-10-08 00:23:17 -0300

    creando input

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+import DataEntryComponent from './Card';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>papi loco</Text>
+      <Text style={styles.titulo}>Bienvenido!!</Text>
+      <Text style={styles.subTitulo}>Ingresa con tu cuenta</Text>
+      <TextInput style={styles.input} placeholder="Usuario" />
+      <TextInput style={styles.input} placeholder="Contraseña" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0fff0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    fontSize:40,
+    fontWeight:"bold",
+    textAlign:"left"
+  },
+  subTitulo:{
+    fontSize:20,
+    color: "gray",
+    textAlign:"left"
+    
+  },
+  input:{
+    borderWidth:1,
+    borderColor:"gray",
+    borderRadius:40,
+    width:"80%",
+    height:40,
+    padding:10,
+    margin:10,
+    textAlign:"left",
+    backgroundColor:"white"
+
+
+  }
+
 });
