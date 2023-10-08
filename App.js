1f87a05a9b7d29d71f85a4432ece1371dd6588df
luis caro <luis.carosalgado1@gmail.com>
2023-10-08 00:59:32 -0300

    aprendiendo y avanzando

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
-import DataEntryComponent from './Card';
+import { StyleSheet, Text, View, TextInput, } from 'react-native';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+     
       <Text style={styles.titulo}>Bienvenido!!</Text>
       <Text style={styles.subTitulo}>Ingresa con tu cuenta</Text>
       <TextInput style={styles.input} placeholder="Usuario" />
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0fff0',
+    backgroundColor: '#d3d3d3',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -40,10 +40,12 @@ const styles = StyleSheet.create({
     width:"80%",
     height:40,
     padding:10,
+    paddingStart:20,
     margin:10,
     textAlign:"left",
     backgroundColor:"white"
-
+  },
+  imagen:{
 
   }
 
